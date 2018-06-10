d3e7675e01d40e3288b0a16dff817daed8159635
Svetlana Ostrovskaya <mn1111@mail.ru>
2018-06-10 12:09:35 +0300

    Added class Hieroglyph

---
+++ TeamProject/Hieroglyph.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject
+{
+    class Hieroglyph
+    {
+        public int Id { get; set; }
+        public string ChineseWord { get; set; }
+        public string Pinyin { get; set; }
+        public string Translation { get; set; }
+    }
+}
